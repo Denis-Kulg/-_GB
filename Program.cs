0949f4778c53d09184aa752845ac75298c6fa01f
Denis <de888n@gmail.com>
2022-02-22 11:38:39 +0300

    SaveProgram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+string[] stringArray = { "1234", "1567", "-2", "computer science" };
+
+void PrintStringArray(string[] stringArray)
+{
+  Write("["); // пятая строка убирает console 
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    Write($"\"{stringArray[i]}\"");
+    if (i != stringArray.Length - 1)
+    {
+      Write(", ");
+    }
+  }
+  Write("]");
+}
+
+int SearchStringWithSizeLessOrEqual3(string[] stringArray)
+{
+  int numberOfString = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if (stringArray[i].Length < 4)
+    {
+      numberOfString++;
+    }
+  }
+  return numberOfString;
+}
+
+string[] CreatArrayWithSizeLessOrEqual3(string[] stringArray, int sizeNewArray)
+{
+  string[] newStringArray = new string[sizeNewArray];
+  int j = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if (stringArray[i].Length < 4)
+    {
+      newStringArray[j] = stringArray[i];
+      j++; 
+    }
+  }
+  return newStringArray;
+}
+
+
+PrintStringArray(stringArray);
+// WriteLine();
+// WriteLine(SearchStringWithSizeLessOrEqual3(stringArray));
+int sizeNewArray = SearchStringWithSizeLessOrEqual3(stringArray); 
+string[] newStringArray = CreatArrayWithSizeLessOrEqual3(stringArray, sizeNewArray); 
+Write(" --> ");
+PrintStringArray(newStringArray);
